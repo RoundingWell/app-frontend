6e2467678d891560c413f547fc26bcf374a19f7e
Paul Falgout <paul.falgout@roundingwell.com>
2024-11-21 15:41:49 +0900

    Prevent routing to a self-stopped app

--- src/js/base/routerapp.js
+++ src/js/base/routerapp.js
@@ -129,7 +129,7 @@ export default App.extend({
   },
 
   startRoute(appName, options) {
-    if (this.isCurrent(appName, options)) {
+    if (this.isCurrent(appName, options) && this.getCurrent().isRunning()) {
       return this.getCurrent().startRoute(this.getCurrentRoute());
     }
     return this.startCurrent(appName, options);
