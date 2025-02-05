3779cc363a56972c57911d3523e9ae7e7e84e1d9
Paul Falgout <paul.falgout@roundingwell.com>
2025-02-05 17:46:15 +0900

    Move patient-field id to construction
    We had added an id accomodation for saving new patient fields prior to having a method for it.  Fields later changed to v5 uuids and a stable id, so it makes more sense to construct them with the eventual id, so that backbone.store caches the correct model

--- src/js/entities-service/entities/patient-fields.js
+++ src/js/entities-service/entities/patient-fields.js
@@ -1,5 +1,4 @@
 import { extend } from 'underscore';
-import { v5 as uuid } from 'uuid';
 import Store from 'backbone.store';
 import BaseCollection from 'js/base/collection';
 import BaseModel from 'js/base/model';
@@ -15,22 +14,12 @@ const _Model = BaseModel.extend({
   getPatient() {
     return this.getRelationship('_patient');
   },
-  isNew() {
-    // NOTE: This will treat the PATCH like a PUT
-    // We won't always have an ID, but never need to POST
-    return false;
-  },
   saveAll(attrs) {
     attrs = extend({}, this.attributes, attrs);
-    const patient = this.toRelation(attrs._patient);
 
-    // NOTE: sets the id instead of attrs.id due to how backbone's save works
-    /* istanbul ignore next: Currently not saving new fields, but would be important if we do */
-    if (!attrs.id) {
-      this.set({ id: uuid(`resource:field:${ attrs.name.toLowerCase() }`, patient.id) });
-    }
-
-    const relationships = { patient };
+    const relationships = {
+      patient: this.toRelation(attrs._patient),
+    };
 
     return this.save(attrs, { relationships }, { wait: true });
   },
