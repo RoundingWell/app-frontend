964a7735b92146d8879eb3008019449a16e06e67
Nick Major <nick.major@roundingwell.com>
2025-03-13 11:43:51 -0500

    Add Find in List to Dashboards list page

--- src/js/views/dashboards/dashboards-all_views.js
+++ src/js/views/dashboards/dashboards-all_views.js
@@ -1,9 +1,11 @@
+import { every } from 'underscore';
 import Radio from 'backbone.radio';
 import hbs from 'handlebars-inline-precompile';
 import { View, CollectionView } from 'marionette';
 
-import PreloadRegion from 'js/regions/preload_region';
+import buildMatchersArray from 'js/utils/formatting/build-matchers-array';
 
+import PreloadRegion from 'js/regions/preload_region';
 
 const ItemView = View.extend({
   template: hbs`
@@ -28,11 +30,54 @@ const EmptyView = View.extend({
   `,
 });
 
+const EmptyFindInListView = View.extend({
+  tagName: 'tr',
+  template: hbs`
+    <td class="table-empty-list">
+      <h2>{{ @intl.dashboards.dashboardsAllViews.emptyFindInListView.noResults }}</h2>
+    </td>
+  `,
+});
+
 const ListView = CollectionView.extend({
   childView: ItemView,
   className: 'table-list',
   tagName: 'table',
-  emptyView: EmptyView,
+  emptyView() {
+    if (this.collection.length && this.state.get('searchQuery')) {
+      return EmptyFindInListView;
+    }
+
+    return EmptyView;
+  },
+  childViewTriggers: {
+    'render': 'listItem:render',
+  },
+  initialize({ state }) {
+    this.state = state;
+
+    this.listenTo(state, 'change:searchQuery', this.searchList);
+  },
+  onAttach() {
+    this.searchList(null, this.state.get('searchQuery'));
+  },
+  onListItemRender(view) {
+    view.searchString = view.$el.text();
+  },
+  searchList(state, searchQuery) {
+    if (!searchQuery) {
+      this.removeFilter();
+      return;
+    }
+
+    const matchers = buildMatchersArray(searchQuery);
+
+    this.setFilter(function({ searchString }) {
+      return every(matchers, function(matcher) {
+        return matcher.test(searchString);
+      });
+    });
+  },
 });
 
 const LayoutView = View.extend({
@@ -43,6 +88,7 @@ const LayoutView = View.extend({
       <div class="flex list-page__title-filter">
         <span class="list-page__title-icon">{{far "gauge"}}</span>{{ @intl.dashboards.dashboardsAllViews.layoutView.title }}
       </div>
+      <div class="u-margin--l-auto" data-search-region></div>
     </div>
   </div>
   <div class="flex-region list-page__list">
@@ -53,6 +99,7 @@ const LayoutView = View.extend({
   </div>
   `,
   regions: {
+    search: '[data-search-region]',
     list: {
       el: '[data-list-region]',
       regionClass: PreloadRegion,
--- test/integration/dashboards/dashboards-all.js
+++ test/integration/dashboards/dashboards-all.js
@@ -61,4 +61,95 @@ context('dashboards all list', function() {
       .find('.table-empty-list')
       .contains('No Dashboards');
   });
+
+  specify('find in list', function() {
+    const testDashboards = [
+      getDashboard({
+        attributes: { name: 'Daily Dashboards' },
+      }),
+      getDashboard({
+        attributes: { name: 'Weekly Dashboard' },
+      }),
+    ];
+
+    cy
+      .routeDashboards(fx => {
+        fx.data = testDashboards;
+
+        return fx;
+      })
+      .routeDashboard(fx => {
+        fx.data = testDashboards[0];
+
+        return fx;
+      })
+      .visit('/dashboards')
+      .wait('@routeDashboards');
+
+    cy
+      .get('.list-page__header')
+      .find('[data-search-region] .js-input')
+      .as('listSearch')
+      .type('abc');
+
+    cy
+      .get('.list-page__header')
+      .find('[data-search-region] .list-search__container')
+      .should('have.class', 'is-applied');
+
+    cy
+      .get('.list-page__list')
+      .as('dashboardList')
+      .find('.table-empty-list')
+      .should('contain', 'No results match your Find in List search');
+
+    cy
+      .get('@listSearch')
+      .next()
+      .should('have.class', 'js-clear')
+      .click();
+
+    cy
+      .get('.list-page__header')
+      .find('[data-search-region] .list-search__container')
+      .should('not.have.class', 'is-applied');
+
+    cy
+      .get('@dashboardList')
+      .find('.table-list__item')
+      .should('have.length', 2);
+
+    cy
+      .get('@listSearch')
+      .next()
+      .should('not.be.visible');
+
+    cy
+      .get('@listSearch')
+      .type('daily');
+
+    cy
+      .get('@dashboardList')
+      .find('.table-list__item')
+      .should('have.length', 1)
+      .first()
+      .should('contain', 'Daily Dashboards')
+      .click()
+      .wait('@routeDashboard');
+
+    cy
+      .go('back')
+      .wait('@routeDashboards');
+
+    cy
+      .get('@listSearch')
+      .should('have.attr', 'value', 'daily');
+
+    cy
+      .get('@dashboardList')
+      .find('.table-list__item')
+      .should('have.length', 1)
+      .first()
+      .should('contain', 'Daily Dashboards');
+  });
 });
