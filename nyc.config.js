601c7fd9731c4fc4a8c5031abdd72c8e8d74665d
Paul Falgout <paul.falgout@roundingwell.com>
2025-06-19 03:03:04 +0900

    Convert nyc config to require friendly format
    https://github.com/cypress-io/code-coverage/issues/668
    
    Cypress coverage won’t load a cjs, and switching to eslint-stylistic was enforcing ESM

--- nyc.config.js
+++
@@ -1,8 +0,0 @@
-import { COVER_INCLUDE, COVER_EXCLUDE } from './config/coverage.js';
-
-export default {
-  'report-dir': './coverage',
-  'include': COVER_INCLUDE,
-  'exclude': COVER_EXCLUDE,
-  'extension': ['.js'],
-};
