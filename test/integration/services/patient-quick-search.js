c51115e2d085692fc9b0977b019338e8d43f95d5
Nick Major <nick.major@roundingwell.com>
2025-08-22 14:20:44 -0500

    Add <strong></strong> to pt search results dob column if 'results.match.label === 'Birth Date'

--- test/integration/services/patient-quick-search.js
+++ test/integration/services/patient-quick-search.js
@@ -11,12 +11,13 @@ context('Patient Quick Search', function() {
         attributes: {
           first_name: 'Test',
           last_name: `${ index } Patient`,
+          birth_date: '2008-01-16',
           status: index % 2 ? 'inactive' : 'active',
         },
       });
     });
 
-    const data = _.map(patients, patient => {
+    const searchResults = _.map(patients, patient => {
       const { first_name, last_name, birth_date, status } = patient.attributes;
 
       return {
@@ -50,7 +51,7 @@ context('Patient Quick Search', function() {
         }
         req.reply({
           body: {
-            data,
+            data: searchResults,
             included: [...getResource(patients, 'patients')],
           },
           delay: 300,
@@ -250,6 +251,32 @@ context('Patient Quick Search', function() {
     cy
       .get('@searchModal')
       .should('contain', 'Search by');
+
+    cy.intercept({
+      method: 'GET',
+      url: '/api/patients?filter*',
+    }, {
+      body: {
+        data: [{
+          ...searchResults[0],
+          attributes: { ...searchResults[0].attributes, match: { label: 'Birth Date', value: '2008-01-16' } },
+        }],
+        included: [getResource(patients[0], 'patients')],
+      },
+      delay: 300,
+    }).as('routePatientSearch');
+
+    cy
+      .get('@searchModal')
+      .find('.patient-search__input')
+      .clear()
+      .type('01/16/2008')
+      .wait('@routePatientSearch');
+
+    cy
+      .get('@searchModal')
+      .find('.js-picklist-item strong')
+      .should('contain', '01/16/2008');
 
     cy
       .go('back');
