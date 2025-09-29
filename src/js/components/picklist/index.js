3b0457e029e8812cc8ed931bf6894b9179ce1e40
Nick Major <nick.major@roundingwell.com>
2025-09-29 14:41:56 -0500

    In picklist only show loader animation if this.ui.loading[0] exists. And clean up animation on destroy

--- src/js/components/picklist/index.js
+++ src/js/components/picklist/index.js
@@ -68,12 +68,20 @@ const PicklistEmpty = View.extend({
     return this.options;
   },
   onRender() {
-    animate(this.ui.loading[0], {
-      opacity: { to: 1, duration: 400 },
-      loop: Infinity,
-      ease: 'inOutSine',
-      alternate: true,
-    });
+    if (this.getOption('isLoading')) {
+      this._loadingAnimation = animate(this.ui.loading[0], {
+        opacity: { to: 1, duration: 400 },
+        loop: Infinity,
+        ease: 'inOutSine',
+        alternate: true,
+      });
+    }
+  },
+  onDestroy() {
+    if (this._loadingAnimation) {
+      this._loadingAnimation.cancel();
+      this._loadingAnimation = null;
+    }
   },
 });
 
