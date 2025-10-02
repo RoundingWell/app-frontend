65beb632f7b70e98ef9c0e383d79e2dfaf570cbe
Paul Falgout <paul.falgout@roundingwell.com>
2025-10-03 00:29:08 +0900

    Add care-ops-five9 package

---
+++ packages/care-ops-five9/five9_app.js
@@ -0,0 +1,81 @@
+import Radio from 'backbone.radio';
+import dayjs from 'dayjs';
+import { applicationApi, crmApi, interactionApi } from './sdk/index';
+
+import App from 'js/base/app';
+
+import { LayoutView } from './five9_views';
+
+const application = applicationApi();
+const crm = crmApi();
+const interaction = interactionApi();
+
+export default App.extend({
+  stateEvents: {
+    'change:isLoggedIn': 'onLoginChange',
+  },
+  initialize({ providerName }) {
+    this.registerApi(providerName);
+    this.handleLogin();
+    this.subscribe();
+  },
+  startAfterInitialized: true,
+  onStart() {
+    this.showView(new LayoutView({ model: this.getState() }));
+  },
+  onLoginChange() {
+    this._call();
+  },
+  registerApi(providerName) {
+    crm.registerApi({
+      getAdtConfig() {
+        return Promise.resolve({ providerName });
+      },
+    });
+  },
+  handleLogin() {
+    application.subscribe({
+      loginStateChanged: ({ state }) => {
+        this.setState('isLoggedIn', state === 'WORKING');
+      },
+    });
+  },
+  subscribe() {
+    interaction.subscribe({
+      callStarted: async({ callData }) => {
+        this.setState('isCalling', true);
+        // TODO: Waiting on client for cav id
+        // const interactionId = callData.interactionId;
+        // await interaction.setCav({
+      },
+      callAccepted: () => {
+        this.setState('callTime', dayjs());
+      },
+      callEnded: () => {
+        this.setState('callTime', null);
+      },
+      callFinished: data => {
+        this.setState('isCalling', false);
+        Radio.request('dialer', 'five9CallComplete', data);
+      },
+    });
+  },
+  call(number, action) {
+    this.setState('isOpen', true);
+
+    // If there's an active call, only show the panel
+    if (this.getState('isCalling')) return;
+
+    this.setState('pendingCall', number);
+    this.setState('action', action.id);
+
+    this._call();
+  },
+  _call() {
+    // If user is not logged in, do not proceed
+    const number = this.getState('pendingCall');
+    if (!this.getState('isLoggedIn') || !number) return;
+    interaction.click2dial({ click2DialData: { clickToDialNumber: number } });
+    this.setState('pendingCall', null);
+  },
+});
--- packages/care-ops-five9/index.js
+++ packages/care-ops-five9/index.js
@@ -1,1 +1,13 @@
-// Radio.request('settings', 'get', 'five9_enabled')
+import Five9App from './five9_app';
+
+let dialerApp;
+
+function call(number, action) {
+  dialerApp.call(number, action);
+}
+
+function init({ region, providerName }) {
+  dialerApp = new Five9App({ region, providerName });
+}
+
+export { call, init };
