5e07dbbb17c6ea4e6e866b8335569b6a3c6ebb8f
Paul Falgout <paul.falgout@roundingwell.com>
2025-10-03 00:28:43 +0900

    Add dialer service

---
+++ src/js/services/dialer.cy.js
@@ -0,0 +1,58 @@
+import { v5 as uuid } from 'uuid';
+import Radio from 'backbone.radio';
+import { RWELL_NS } from 'js/static';
+import DialerService from './dialer';
+
+context('Dialer Service', function() {
+  beforeEach(function() {
+    cy
+      .clock()
+      .mount(rootView => {
+        const region = rootView.getRegion('overlay');
+        new DialerService({ region });
+
+        return '<div></div>';
+      })
+      .as('root');
+  });
+
+  specify('five9CallComplete', function() {
+    cy
+      .intercept('PATCH', '/api/artifacts/**', {
+        statusCode: 201,
+        body: {
+          data: {},
+        },
+      })
+      .as('patchArtifact');
+
+    cy
+      .get('@root')
+      .then(() => {
+        Radio.request('dialer', 'five9CallComplete', {
+          sessionID: 'abc1234',
+          callDuration: 42,
+          disposition: 'completed',
+        });
+      });
+
+    cy
+      .wait('@patchArtifact')
+      .its('request.body')
+      .should('deep.equal', {
+        data: {
+          type: 'artifacts',
+          id: uuid('five9-call-log:abc1234', RWELL_NS),
+          attributes: {
+            artifact: 'five9-call-log',
+            identifier: 'abc1234',
+            values: {
+              sessionID: 'abc1234',
+              callDuration: 42,
+              disposition: 'completed',
+            },
+          },
+        },
+      });
+  });
+});
