b58e2fe3e4938f729491d48a2fc63c64bb0313f9
Paul Falgout <paul.falgout@roundingwell.com>
2026-04-15 05:22:30 +0900

    Restore programs-all coverage regression test

--- test/integration/programs/programs-all.js
+++ test/integration/programs/programs-all.js
@@ -4,47 +4,81 @@ import { getProgram } from 'support/api/programs';
 
 context('program all list', function() {
   specify('display programs list', function() {
+    const firstProgram = getProgram({
+      attributes: {
+        name: 'First in List',
+        published_at: testTs(),
+        archived_at: null,
+        updated_at: testTs(),
+      },
+    });
+
+    const lastProgram = getProgram({
+      attributes: {
+        name: 'Last in List',
+        published_at: testTs(),
+        archived_at: null,
+        updated_at: testTsSubtract(2),
+      },
+    });
+
+    const secondProgram = getProgram({
+      attributes: {
+        name: 'Second in List, Not Published',
+        published_at: null,
+        archived_at: null,
+        updated_at: testTsSubtract(1),
+      },
+    });
+
     cy
-      .routePrograms(fx => {
-        fx.data = [
-          getProgram({
-            attributes: {
-              name: 'First in List',
-              published_at: testTs(),
-              archived_at: null,
-              updated_at: testTs(),
-            },
-          }),
-          getProgram({
-            attributes: {
-              name: 'Last in List',
-              published_at: testTs(),
-              archived_at: null,
-              updated_at: testTsSubtract(2),
-            },
-          }),
-          getProgram({
-            attributes: {
-              name: 'Second in List, Not Published',
-              published_at: null,
-              archived_at: null,
-              updated_at: testTsSubtract(1),
-            },
-          }),
-        ];
+      .routeWorkspacePrograms(fx => {
+        fx.data = [firstProgram, lastProgram, secondProgram];
 
         return fx;
       })
+      .routePrograms(fx => {
+        fx.data = [firstProgram, lastProgram, secondProgram];
+
+        return fx;
+      })
+      .routeProgram(fx => {
+        fx.data = firstProgram;
+
+        return fx;
+      })
+      .routeProgramActions()
+      .routeProgramFlows()
       .visit('/programs')
       .wait('@routePrograms');
 
     cy
+      .get('.table-list__item', { timeout: 10000 })
+      .should('have.length', 3);
+
+    cy
       .get('.table-list__item')
       .first()
-      .should('contain', 'First in List')
-      .should('contain', 'On')
-      .next()
-      .should('contain', 'Second in List, Not Published')
-      .should('contain', 'Off');
+      .within(() => {
+        cy.get('[data-testid="program-list-name"]').should('contain', 'First in List');
+        cy.contains('On');
+      });
+
+    cy
+      .get('.table-list__item')
+      .eq(1)
+      .within(() => {
+        cy.get('[data-testid="program-list-name"]').should('contain', 'Second in List, Not Published');
+        cy.contains('Off');
+      });
+
+    cy
+      .get('.table-list__item')
+      .first()
+      .click();
+
+    cy
+      .location('pathname', { timeout: 10000 })
+      .should('contain', `/program/${ firstProgram.id }`);
   });
 });
