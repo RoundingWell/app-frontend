dbff799bc20ea6282d112a731d9c41b3ac5e0790
Paul Falgout <paul.falgout@roundingwell.com>
2026-09-23 15:05:25 +0900

    refactor(five9): scope SDK callbacks and lookups to the active run

--- packages/care-ops-five9/five9_app.js
+++ packages/care-ops-five9/five9_app.js
@@ -1,6 +1,7 @@
 import { get } from 'underscore';
-import { Radio } from 'marionette';
+import Backbone from 'backbone';
 import dayjs from 'dayjs';
+import { Radio } from 'marionette';
 import fetcher, { handleJSON } from 'js/base/fetch';
 import { applicationApi, crmApi, interactionApi } from './sdk/index';
 
@@ -16,18 +17,31 @@ export default App.extend({
   stateEvents: {
     'change:isLoggedIn': 'onLoginChange',
   },
+  createState() {
+    return new Backbone.Model();
+  },
   initialize({ providerName, patients }) {
     this.patients = patients;
     this.registerApi(providerName);
     this.handleLogin();
     this.subscribe();
   },
-  startAfterInitialized: true,
   onStart() {
+    this._abortController?.abort();
+    this._abortController = new AbortController();
+
     this.showView(new LayoutView({
       model: this.getState(),
       collection: this.patients,
     }));
+
+    this._call();
+  },
+  onStop() {
+    this._abortController?.abort();
+  },
+  onBeforeDestroy() {
+    this._abortController?.abort();
   },
   onLoginChange() {
     this._call();
@@ -42,83 +56,102 @@ export default App.extend({
   handleLogin() {
     application.subscribe({
       loginStateChanged: ({ state }) => {
-        this.setState('isLoggedIn', state === 'WORKING');
+        this.getState().set('isLoggedIn', state === 'WORKING');
       },
     });
   },
   subscribe() {
+    const whenRunning = method => (...args) => {
+      if (!this.isRunning()) return;
+      return method.apply(this, args);
+    };
+
     interaction.subscribe({
-      callStarted: async({ callData }) => {
-        this.setState('isCalling', true);
-        const actionId = this.getState('actionId');
-
-        if (!actionId) return;
-
-        Radio.request('dialer', 'five9Call', { callData, actionId });
-
-        await interaction.setCav({
-          interactionId: callData.interactionId,
-          cavList: [
-            {
-              id: '300000000000151',
-              value: actionId,
-            },
-          ],
-        });
-      },
-      callAccepted: async({ callData }) => {
-        this.setState('callTime', dayjs());
-
-        const number = await this._getCallNumber(callData);
-
-        Radio.request('dialer', 'showPatientLinks', { actionId: this.getState('actionId'), number });
-      },
-      callEnded: () => {
-        this.setState({
-          isTransferredCall: null,
-          callTime: null,
-        });
-
-        Radio.request('dialer', 'showPatientLinks', null);
-      },
-      callFinished: ({ callLogData, callData }) => {
-        this.setState({
-          isCalling: false,
-          actionId: null,
-          callTime: null,
-          isTransferredCall: null,
-        });
-
-        Radio.request('dialer', 'five9Call', { callData, callLogData });
-        Radio.request('dialer', 'showPatientLinks', null);
-      },
+      callStarted: whenRunning(this.onCallStarted),
+      callAccepted: whenRunning(this.onCallAccepted),
+      callEnded: whenRunning(this.onCallEnded),
+      callFinished: whenRunning(this.onCallFinished),
     });
   },
+  async onCallStarted({ callData }) {
+    const state = this.getState();
+    state.set('isCalling', true);
+    const actionId = state.get('actionId');
+
+    if (!actionId) return;
+
+    Radio.request('dialer', 'five9Call', { callData, actionId });
+
+    await interaction.setCav({
+      interactionId: callData.interactionId,
+      cavList: [
+        {
+          id: '300000000000151',
+          value: actionId,
+        },
+      ],
+    });
+  },
+  async onCallAccepted({ callData }) {
+    const { signal } = this._abortController;
+    const state = this.getState();
+    state.set('callTime', dayjs());
+
+    try {
+      const number = await this._getCallNumber(callData, signal);
+      if (signal.aborted) return;
+
+      Radio.request('dialer', 'showPatientLinks', { actionId: state.get('actionId'), number });
+    } catch(error) {
+      if (!signal.aborted) throw error;
+    }
+  },
+  onCallEnded() {
+    this.getState().set({
+      isTransferredCall: null,
+      callTime: null,
+    });
+
+    Radio.request('dialer', 'showPatientLinks', null);
+  },
+  onCallFinished({ callLogData, callData }) {
+    this.getState().set({
+      isCalling: false,
+      actionId: null,
+      callTime: null,
+      isTransferredCall: null,
+    });
+
+    Radio.request('dialer', 'five9Call', { callData, callLogData });
+    Radio.request('dialer', 'showPatientLinks', null);
+  },
   call(number, action) {
-    this.setState('isOpen', true);
+    const state = this.getState();
+    state.set('isOpen', true);
 
     // If there's an active call, only show the panel
-    if (this.getState('isCalling')) return;
+    if (state.get('isCalling')) return;
 
-    this.setState('pendingCall', number);
-    this.setState('actionId', action.id);
+    state.set({ pendingCall: number, actionId: action.id });
 
     this._call();
   },
   _call() {
     // If user is not logged in, do not proceed
-    const number = this.getState('pendingCall');
-    if (!this.getState('isLoggedIn') || !number) return;
+    const state = this.getState();
+    const number = state.get('pendingCall');
+    if (!state.get('isLoggedIn') || !number) return;
     interaction.click2dial({ click2DialData: { clickToDialNumber: number } });
-    this.setState('pendingCall', null);
+    state.set('pendingCall', null);
   },
-  async _getCallNumber(callData) {
+  async _getCallNumber(callData, signal) {
     const { number } = callData;
 
     if (number.includes('agent:')) {
-      this.setState('isTransferredCall', true);
+      this.getState().set('isTransferredCall', true);
 
-      const { data } = await fetcher('/api/artifacts', {
+      const response = await fetcher('/api/artifacts', {
+        signal,
         data: {
           filter: {
             type: 'five9-call-log',
@@ -128,8 +161,9 @@ export default App.extend({
           },
         },
       }).then(handleJSON);
+      if (!response) return;
 
-      return get(data, ['0', 'attributes', 'values', 'callData', 'number']);
+      return get(response, ['data', '0', 'attributes', 'values', 'callData', 'number']);
     }
 
     return number;
